f1bf797f7fad0b23d15a77d7733a93959934d4b9
duydoan169 <hanh250470@gmail.com>
2025-09-08 22:54:21 +0700

    btvn

---
+++ session24/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Main from '../components/HN_KS24CNTT4_DoanNgocDuy_006/Main'
+export default function App() {
+  return (
+    <div>
+      <Main></Main>
+    </div>
+  )
+}
